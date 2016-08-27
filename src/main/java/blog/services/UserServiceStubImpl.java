06c63e185b523eb30f44a5ff06548f708547fa8f
Dilyana <diljkap@gmail.com>
2016-08-27 23:06:16 +0300

    JPA implementations for Post/User & "blog_db" database link

--- src/main/java/blog/services/UserServiceStubImpl.java
+++ src/main/java/blog/services/UserServiceStubImpl.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 import java.util.Objects;
 
 @Service
-public class UserServiceStubImpl implements UserService {
+public abstract class UserServiceStubImpl implements UserService {
     @Override
     public boolean authenticate(String username, String password) {
         //sample password check (username == password)

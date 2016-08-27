4e6403095cfab0923624bfdd36fe2b2db7a0ccde
Dilyana <diljkap@gmail.com>
2016-08-27 16:36:37 +0300

    Refactoring UsersController and adding logout redirect

--- src/main/java/blog/controllers/UsersController.java
+++ src/main/java/blog/controllers/UsersController.java
@@ -1,6 +1,7 @@
 package blog.controllers;
 
 import blog.forms.LoginForm;
+import blog.forms.RegisterForm;
 import blog.services.NotificationService;
 import blog.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import javax.validation.Valid;
 
 @Controller
-public class LoginController {
+public class UsersController {
     @Autowired
     private UserService userService;
 
@@ -34,11 +35,39 @@ public class LoginController {
             notifyService.addErrorMessage("Invalid login!");
             return "users/login";
         }
+        //TO DO
         notifyService.addInfoMessage("Login successful.");
         return "redirect:/";
     }
-}
 
+    @RequestMapping("/users/register")
+    public String register(RegisterForm registerForm) {
+        return "users/register";
+    }
+
+    @RequestMapping(value = "/users/register", method = RequestMethod.POST)
+    public String registerPage(@Valid RegisterForm registerForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            notifyService.addErrorMessage("Please fill the form correctly!");
+            return "users/register";
+        }
+
+        if (!userService.registerValidation(
+                registerForm.getUsername())) {
+            notifyService.addErrorMessage("Invalid registration!");
+            return "users/register";
+        }
+        //TO DO
+        notifyService.addInfoMessage("Registration successful");
+        return "redirect:/";
+    }
+
+    @RequestMapping("/users/logout")
+    public String logout() {
+        //TO DO
+        return "redirect:/";
+    }
+}
 
 
 
